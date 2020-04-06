aa11421f730f32f202dbc37ff3b2fe109e5be7f8
sjwy2k <32915656+sjwy2k@users.noreply.github.com>
2020-04-06 14:22:24 +0900

    #200406 SJW close(conn) 추가

--- 20_StudyTogether_AM/src/com/kh/buy/controller/BuySuccessServlet.java
+++ 20_StudyTogether_AM/src/com/kh/buy/controller/BuySuccessServlet.java
@@ -1,11 +1,15 @@
 package com.kh.buy.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.cart.model.vo.Cart;
 
 /**
  * Servlet implementation class BuySuccessServlet
@@ -26,7 +30,13 @@ public class BuySuccessServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Cart> list=(List)request.getAttribute("list");
 		
+		System.out.println(list.get(0));
+		
+		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/member/myPage.jsp").forward(request, response);
 	}
 
 	/**
